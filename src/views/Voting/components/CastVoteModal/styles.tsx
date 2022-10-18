aba6a1cafbdc574ad88659386f8c44384e5a325b
kailun <Lukailun820820@outlook.com>
2022-10-18 20:37:07 +0800

    update styles.txs

--- src/views/Voting/components/CastVoteModal/styles.tsx
+++ src/views/Voting/components/CastVoteModal/styles.tsx
@@ -8,7 +8,7 @@ export const VotingBoxBorder = styled(Box)<{ hasBoosted?: boolean }>`
   border: ${({ hasBoosted }) => (hasBoosted ? '0px' : '1px')};
   border-style: solid;
   border-color: ${({ theme }) => (theme.colors.cardBorder)};
-  background: 'transparent';
+  background: transparent;
 `
 //border-color: ${({ theme, hasBoosted }) => (hasBoosted ? 'transparent' : theme.colors.cardBorder)};
 //background: ${({ hasBoosted }) => (hasBoosted ? 'linear-gradient(180deg, #53dee9, #7645d9)' : 'transparent')};
@@ -19,7 +19,7 @@ export const VotingBoxCardInner = styled(Box)<{ hasBoosted?: boolean }>`
   align-items: center;
   border-radius: 12px;
   justify-content: space-between;
-  background: ${({}) => ('transparent')};
+  background: transparent;
 `
 //background: ${({ theme, hasBoosted }) => (hasBoosted ? theme.colors.gradientBubblegum : 'transparent')};
 export const ModalInner: React.FC<React.PropsWithChildren<BoxProps>> = (props) => {
