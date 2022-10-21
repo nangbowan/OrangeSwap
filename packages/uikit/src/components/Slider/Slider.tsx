5a699c2deecd0f25ac3f072c18e866d0c602c204
april <hesiyuetian@gmail.com>
2022-10-21 21:45:08 +0800

    update: slider style

--- packages/uikit/src/components/Slider/Slider.tsx
+++ packages/uikit/src/components/Slider/Slider.tsx
@@ -45,7 +45,7 @@ const Slider: React.FC<React.PropsWithChildren<SliderProps>> = ({
   const displayValueLabel = isMax ? "MAX" : valueLabel;
   return (
     <Box position="relative" height="48px" {...props}>
-      <BunnyButt disabled={disabled} />
+      {/* <BunnyButt disabled={disabled} /> */}
       <BunnySlider>
         <BarBackground disabled={disabled} />
         <BarProgress style={{ width: progressWidth }} disabled={disabled} />
