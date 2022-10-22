f1e1e10d73366a80ae1ffbe4cc194e13155637e3
kailun <Lukailun820820@outlook.com>
2022-10-23 00:50:53 +0800

    update error

--- packages/tokens/src/201022.ts
+++ packages/tokens/src/201022.ts
@@ -1,18 +1,10 @@
 import { ChainId ,Token,WFON} from '@pancakeswap/sdk'
-<<<<<<< HEAD
-import { USDT_FON , ORG_MAINNET} from './common'
-=======
 import { USDT_FON, ORG_MAINNET } from './common'
 
->>>>>>> 2cf3f3cae476bd9ca0fd1aca68edeb4fc897e99a
 export const fonTokens = {
     wfon: WFON[ChainId.FON],
     // bnb here points to the wbnb contract. Wherever the currency BNB is required, conditional checks for the symbol 'BNB' can be used
     fon: ORG_MAINNET,
     usdt:USDT_FON,
-<<<<<<< HEAD
     org:ORG_MAINNET,
-    
-=======
->>>>>>> 2cf3f3cae476bd9ca0fd1aca68edeb4fc897e99a
   }
