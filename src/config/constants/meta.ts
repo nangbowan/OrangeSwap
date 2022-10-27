ef0ae979f459432a6e244e144ac9a1541f60cd1c
kailun <Lukailun820820@outlook.com>
2022-10-27 18:26:17 +0800

    update meta

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -5,7 +5,7 @@ import { PageMeta } from './types'
 export const DEFAULT_META: PageMeta = {
   title: 'OrangeSwap',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+    'OrangeSwap-A next evolution DeFi exchange on FON Smart Chain(FSC)  The most popular AMM on FSC! Earn ORG through yield farming or win it by trading, then stake it in Pools to earn more tokens! Initial Farm Offerings(new token launch model pioneered by OrangeSwap)NFTs, and more, on a platform you can trust.',
   image: 'https://orangeswap.org/images/base/banner2.png',
 }
 
