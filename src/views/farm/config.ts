93b9ec9043167e96665b70d6169a91c8015f67bb
april <hesiyuetian@gmail.com>
2022-10-30 09:51:25 +0800

    update: farm config

--- src/views/farm/config.ts
+++ src/views/farm/config.ts
@@ -15,5 +15,15 @@ export const farmConfig = {
         symbolBddress: '0xd379CcF60eaec5E0B426ec8E7fF55faE1fDaDdc5',
         apy: '30',
         rewardSymbol: 'ORG'
+    },
+
+    // Fon Main Chain
+    "0x05c7D64dC0B77fF7E52e36F19e7d6f451E18d3De": {
+        symbolA: 'WBNB',
+        symbolAddress: '0xc91808756733BB10106e6F3717375047FD5E3D16',
+        symbolB: 'USDT',
+        symbolBddress: '0x9a9eD7440a3850c4D7240c9FcA8B7C96802615f0',
+        apy: '30',
+        rewardSymbol: 'ORG'
     }
 }
